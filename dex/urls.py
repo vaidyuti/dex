bb9bc085bb8cb0fb201959487d104c8fd2f3d309
Rithvik Nishad <rithvikn2001@gmail.com>
2022-08-28 12:43:17 +0530

    🧑‍✈️️ Configure JWT Auth, OpenAPI, Swagger, Redocly 🚀️

--- dex/urls.py
+++ dex/urls.py
@@ -1,10 +1,26 @@
 from django.contrib import admin
-from django.urls import include, path
-from rest_framework import routers
+from django.urls import include, path, re_path
+from rest_framework import routers, permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from p2p.views import BuyViewSet, SellViewSet, TradeViewSet
 from prosumer.views import ProsumerViewSet
 from users.views import UserViewSet
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Vaidyuti DEX API",
+        default_version="v1",
+        description="Vaidyuti DEX API for P2P trading",
+        #   terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@email.com"),
+        license=openapi.License(name="License Unknown"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 router = routers.DefaultRouter()
 router.register(r"prosumer/prosumers", ProsumerViewSet)
@@ -18,7 +34,13 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/docs/auth/", include("rest_framework.urls", namespace="rest_framework")),
     path("api/v1/auth/", include("dj_rest_auth.urls")),
+    path("api/v1/auth/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/v1/auth/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/v1/auth/register/", include("dj_rest_auth.registration.urls")),
     path("api/v1/", include("openapi.urls")),
     path("api/v1/", include(router.urls)),
+    # Swagger, Redoc and OpenAPI
+    re_path(r"^swagger(?P<format>\.json|\.yaml)$", schema_view.without_ui(cache_timeout=0), name="schema-json"),
+    re_path(r"^swagger/$", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+    re_path(r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
