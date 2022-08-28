e049bc642f0b169d586a28863177c4229bb61476
Rithvik Nishad <rithvikn2001@gmail.com>
2022-08-28 14:13:37 +0530

    🧑‍✈️️ Fix router prefix typo 🚀️

--- dex/urls.py
+++ dex/urls.py
@@ -24,8 +24,8 @@ schema_view = get_schema_view(
 
 router = routers.DefaultRouter()
 router.register(r"prosumer/prosumers", ProsumerViewSet)
-router.register(r"p2p/buies", BuyViewSet)
-router.register(r"p2p/sells", SellViewSet)
+router.register(r"p2p/buys", BuyViewSet)
+router.register(r"p2p/sell", SellViewSet)
 router.register(r"p2p/trades", TradeViewSet)
 router.register(r"users/users", UserViewSet)
 
